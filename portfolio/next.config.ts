b9e480e13d1dfee2d5dc73100c9b07b6a61fb2c0
Big Monkey <OMP35336@gmail.com>
2025-04-11 00:34:52 -0500

    Update next.config.ts

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export"
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/gourdy09.github.io",
+  assetPrefix: "/gourdy09.github.io",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
